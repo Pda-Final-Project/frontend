fa42ac2173b01375c8effff6486099a52bc9db6e
kwak <sal091625@gmail.com>
2025-02-20 17:37:15 +0900

    P3-42(#18) feature/tradeApi 작성

--- src/api/tradeApi.js
+++ src/api/tradeApi.js
@@ -1,1 +1,11 @@
 //매수/매도, 주문 취소, 주문 정정
+import api from "./axiosInstance";
+
+const removeOrder = (orderId) =>
+  api.delete(`/order/${orderId}`, { isAuthRequired: true });
+const updateOrder = (orderId, orderData) =>
+  api.put(`/order/${orderId}`, orderData, { isAuthRequired: true });
+const addOrder = (orderData) =>
+  api.post("/order", { orderData }, { isAuthRequired: true });
+
+export { removeOrder, updateOrder, addOrder };
