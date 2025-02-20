8893746bd84eaed5194e10b4a7be0e6ec122a976
kwak <sal091625@gmail.com>
2025-02-20 16:12:09 +0900

    P3-42(#14) feature/api 기본 파일 생성 및 axiosInstance 작성, authApi 작성

---
+++ src/api/authApi.js
@@ -0,0 +1,12 @@
+//로그인, 회원가입
+import api from "./axiosInstance";
+const login = (phoneNumber, password) =>
+  api.post("/auth/login", { phoneNumber, password }, { isAuthRequired: false });
+const register = (userData) =>
+  api.post("auth/signup", userData, { isAuthRequired: false });
+const logout = () => {
+  sessionStorage.removeItem("accessToken");
+  console.log("로그아웃 완료!");
+};
+
+export { login, register, logout };
---
+++ src/api/axiosInstance.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000,
+  headers: { "Content-Type": "application/json" },
+});
+
+// 요청 인터셉터 - isAuthRequired 플래그 기반 인증 처리
+api.interceptors.request.use((config) => {
+  if (config.isAuthRequired) {
+    const token = sessionStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      // 토큰이 없으면 적절한 처리
+      console.log("토큰이 없습니다.");
+    }
+  }
+
+  return config;
+});
+
+export default api;
