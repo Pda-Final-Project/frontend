2034dfb1341bf85ecb1afc420f54ed6712aab189
kwak <sal091625@gmail.com>
2025-02-20 09:01:03 +0900

    P3-42(#14) feature/사이드바 상태 관리

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,18 +5,28 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div>
-      <div>
+    <div className="flex h-screen w-full">
+      {/* 사이드바 - 왼쪽에 고정 */}
+      <aside className=" bg-gray-800 text-white h-full sticky top-0">
         <Sidebar />
-      </div>
-      <div>
-        <Header />
-        <main>
-          <div>
-            <Outlet />
-          </div>
+      </aside>
+
+      {/* 메인 컨텐츠 영역 */}
+      <div className="flex flex-col flex-grow">
+        {/* 헤더 */}
+        <header className="bg-gray-200">
+          <Header />
+        </header>
+
+        {/* 메인 콘텐츠 */}
+        <main className="flex-grow bg-gray-100 p-6 overflow-y-auto">
+          <Outlet />
         </main>
-        <Footer />
+
+        {/* 푸터 */}
+        <footer className="bg-gray-200">
+          <Footer />
+        </footer>
       </div>
     </div>
   );
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import HoldingSidebar from "./SidebarTab/HoldingStockTab";
+import InterestStockTab from "./SidebarTab/InterestStockTab";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentSidebar, setCurrentSidebar] = useState(""); //none, like, have
+
+  useEffect(() => {
+    console.log(isOpen, currentSidebar);
+  }, [isOpen, currentSidebar]);
+
+  const handleSidebar = (sidebarType) => {
+    if (currentSidebar == sidebarType && isOpen) {
+      setIsOpen(false);
+    } else {
+      setCurrentSidebar(sidebarType);
+      setIsOpen(true);
+    }
+  };
+  return (
+    <div>
+      <div
+        onClick={() => {
+          handleSidebar("like");
+        }}
+      >
+        관심종목
+      </div>
+      <div
+        onClick={() => {
+          handleSidebar("have");
+        }}
+      >
+        보유종목
+      </div>
+      {isOpen ? (
+        currentSidebar === "like" ? (
+          <InterestStockTab />
+        ) : (
+          <HoldingSidebar />
+        )
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
