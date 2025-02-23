ef71c5908e7b3e861fbeb8915fcea26dd007a81a
kwak <sal091625@gmail.com>
2025-02-23 16:33:16 +0900

    P3-47(#18) feature/default 사이드바 css

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import HoldingSidebar from "./SidebarTab/HoldingStockTab";
 import InterestStockTab from "./SidebarTab/InterestStockTab";
-import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { GoGraph } from "react-icons/go";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,6 +13,7 @@ export default function Sidebar() {
   }, [isOpen, currentSidebar]);
 
   const handleSidebar = (sidebarType) => {
+    console.log(sidebarType, isOpen);
     if (currentSidebar === sidebarType && isOpen) {
       setIsOpen(false);
     } else {
@@ -23,20 +25,29 @@ export default function Sidebar() {
   return (
     <>
       {/* 바깥 사이드바 */}
-      <div className="fixed left-0 top-0 h-full w-20 bg-white p-4 border-r-1 border-gray-md">
+      <div className="fixed left-0 top-0 h-full w-20 bg-white p-4 px-2 border-r-1 border-gray-md text-gray-md">
         <div className="space-y-4">
           <div
             onClick={() => handleSidebar("interest")}
-            className="cursor-pointer"
+            className={`flex flex-col justify-center items-center cursor-pointer text-center py-2 text-3xl hover:text-blue-md hover:bg-gray-light rounded-md font-semibold space-y-2 ${
+              currentSidebar == "interest" && isOpen
+                ? "text-blue-md bg-gray-light"
+                : ""
+            }`}
           >
-            <FaRegHeart />
-            관심종목
+            <FaHeart />
+            <div className="text-sm">관심종목</div>
           </div>
           <div
             onClick={() => handleSidebar("holding")}
-            className="cursor-pointer"
+            className={`flex flex-col justify-center items-center cursor-pointer text-center py-2 text-3xl hover:text-blue-md hover:bg-gray-light rounded-md font-semibold space-y-2 ${
+              currentSidebar == "holding" && isOpen
+                ? "text-blue-md bg-gray-light"
+                : ""
+            }`}
           >
-            보유종목
+            <GoGraph />
+            <div className="text-sm">보유종목</div>
           </div>
         </div>
       </div>
