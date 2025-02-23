0df46bba0b3a911718ce3111c9dd9f2833bbc999
kwak <sal091625@gmail.com>
2025-02-23 16:08:47 +0900

    P3-47(#18) fix/레이아웃 깨짐 수정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,26 +5,26 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div className="flex h-screen w-full">
+    <div className="flex h-screen w-full overflow-x-hidden">
       {/* 사이드바 - 왼쪽에 고정 */}
       <aside className="h-full top-0">
         <Sidebar />
       </aside>
 
       {/* 메인 컨텐츠 영역 */}
-      <div className="relative left-20 flex flex-col flex-grow">
+      <div className="flex flex-col flex-grow">
         {/* 헤더 */}
-        <header className="bg-gray-200">
+        <header className="bg-gray-200 fixed top-0 left-20 right-0 z-10 h-16">
           <Header />
         </header>
 
         {/* 메인 콘텐츠 */}
-        <main className="flex-grow bg-gray-100 p-6 overflow-y-auto">
+        <main className="flex flex-grow left-20 relative min-h-screen mt-16">
           <Outlet />
         </main>
 
         {/* 푸터 */}
-        <footer className="bg-gray-200">
+        <footer className="bg-gray-200 h-full">
           <Footer />
         </footer>
       </div>
