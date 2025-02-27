a2cb6acf802d9a93394d15ebefd1ea925b64b771
kwak <sal091625@gmail.com>
2025-02-27 09:04:30 +0900

    P3-72(#27): 공시 좋아요 컴포넌트 구현(API 테스트x)

--- src/pages 2/DisclosureListPage/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function index() {
-  return <div>공시리스트</div>;
-}
--- src/pages 2/MainPage/components/DisclosureTab.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function DisclosureTab() {
-  return <div>DisclosureTab</div>;
-}
