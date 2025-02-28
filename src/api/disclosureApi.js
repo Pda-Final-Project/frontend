bf637711ab57edcf5224463ab2b3b98fa4275470
kwak <sal091625@gmail.com>
2025-02-28 13:23:04 +0900

    P3-82(#37) feature/공시 리스트 api 연동 및 UI, CSS 수정

--- src/api/disclosureApi.js
+++ src/api/disclosureApi.js
@@ -1,1 +1,16 @@
 //공시 리스트 조회, 번역, 요약 조회
+import api from "./axiosInstance";
+
+const fetchFillingInfo = (filling_id) =>
+  api.get(`http://127.0.0.1:19098/v1/api/fillings/${filling_id}`, {
+    isAuthRequired: false,
+  });
+
+const fetchFillings = (params) =>
+  api.get("http://localhost:19098/v1/api/fillings", {
+    params: params, // 빈 값 필터링하여 전달
+    isAuthRequired: false,
+    validateStatus: (status) => status >= 200 && status < 400,
+  });
+
+export { fetchFillingInfo, fetchFillings };
--- src/pages/DisclosureListPage/index.jsx
+++ src/pages/DisclosureListPage/index.jsx
@@ -1,43 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { fetchFillings } from "../../api/disclosureApi";
+
+const fillingTypeData = [
+  { id: "10-Q", name: "분기 보고서" },
+  { id: "8-K", name: "수시 보고서" },
+  { id: "S-1", name: "증권 발행 등록 신고서" },
+  { id: "4", name: "내부자 거래 보고서" },
+  { id: "SC 13G", name: "주식 대량 보유 보고서" },
+];
 
 export default function Index() {
   const navigate = useNavigate();
   const location = useLocation();
 
   // 검색 필터 상태
-  const [searchTerm, setSearchTerm] = useState("");
+  const [ticker, setTicker] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
-  const [selectedReportType, setSelectedReportType] = useState("수시 보고서");
+  const [fillingType, setFillingType] = useState("");
 
-  // 더미 공시 데이터 (API 연동 가능)
-  const DUMMY_REPORTS = Array(15).fill({
-    type: "8-K",
-    title: "테슬라 수시보고서",
-    date: "2025-00-00",
-  });
-
-  // 페이지네이션 상태
+  const [fillings, setFillings] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const reportsPerPage = 5;
+  const reportsPerPage = 10;
 
-  // 현재 페이지에 해당하는 공시 데이터
+  const tryFetchDisclosures = async () => {
+    const params = {
+      ticker,
+      fillingType,
+      startDate,
+      endDate,
+    };
+    const filteredParams = Object.fromEntries(
+      Object.entries(params).filter(([_, value]) => value !== "")
+    );
+    try {
+      const response = await fetchFillings(filteredParams);
+
+      if (response.data.status === "FOUND") {
+        setFillings(response.data.data.content);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSearch = () => {
+    setCurrentPage(1);
+    tryFetchDisclosures();
+  };
+
+  useEffect(() => {
+    tryFetchDisclosures();
+  }, []);
+
   const indexOfLastReport = currentPage * reportsPerPage;
   const indexOfFirstReport = indexOfLastReport - reportsPerPage;
-  const currentReports = DUMMY_REPORTS.slice(
-    indexOfFirstReport,
-    indexOfLastReport
-  );
+  const currentReports = fillings.slice(indexOfFirstReport, indexOfLastReport);
 
   const handleClick = (index) => {
-    if (location.pathname.startsWith("/main")) {
-      // "/main/NVDA/all" → "/main/NVDA/index"
-      navigate(location.pathname.replace("/all", `/${index}`));
-    } else {
-      // 기본적으로 "/disclosures/index"
-      navigate(`${location.pathname}/${index}`);
-    }
+    navigate(`${location.pathname}/${index}`);
+  };
+
+  const resetSearch = () => {
+    setCurrentPage(1);
+    setEndDate("");
+    setStartDate("");
+    setFillingType("");
+    setTicker("");
   };
 
   return (
@@ -45,74 +75,76 @@ export default function Index() {
       {/* 검색 필터 */}
       <h1 className="text-lg font-bold mb-3">해외 공시</h1>
 
-      <div className="bg-gray-100 p-5 rounded-lg">
-        {/* 왼쪽(종목 + 기간) / 오른쪽(공시유형) 배치 */}
-        <div className="flex justify-between gap-5">
+      <div className="bg-gray-100 p-5 rounded-lg flex flex-col justify-center items-center">
+        <div className="flex grid-cols-2 gap-8 w-full">
           {/* 왼쪽 (종목명 + 기간) */}
           <div className="flex flex-col w-1/2 gap-3">
             {/* 종목 검색 */}
             <div className="flex items-center">
-              <h2 className="text-md font-semibold w-16">종목</h2>
+              <h2 className="text-md font-semibold w-1/6">종목</h2>
               <input
                 type="text"
-                placeholder="종목명을 입력하세요"
-                className="bg-white p-2 w-full rounded-md"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder="종목코드를 입력하세요"
+                className="bg-white p-2 w-5/6 rounded-md"
+                value={ticker}
+                onChange={(e) => setTicker(e.target.value)}
               />
             </div>
 
             {/* 기간 선택 */}
             <div className="flex items-center">
-              <h2 className="text-md font-semibold w-16">기간</h2>
-              <input
-                type="date"
-                className="bg-white p-2 rounded-md flex-grow"
-                value={startDate}
-                onChange={(e) => setStartDate(e.target.value)}
-              />
-              <span className="px-3">~</span>
-              <input
-                type="date"
-                className="bg-white p-2 rounded-md flex-grow"
-                value={endDate}
-                onChange={(e) => setEndDate(e.target.value)}
-              />
+              <h2 className="text-md font-semibold w-1/6">기간</h2>
+              <div className="w-5/6 flex items-center">
+                <input
+                  type="date"
+                  className="bg-white p-2 rounded-md"
+                  value={startDate}
+                  onChange={(e) => setStartDate(e.target.value)}
+                />
+                <span className="px-3">-</span>
+                <input
+                  type="date"
+                  className="bg-white p-2 rounded-md"
+                  value={endDate}
+                  onChange={(e) => setEndDate(e.target.value)}
+                />
+              </div>
             </div>
           </div>
 
           {/* 오른쪽 (공시유형) */}
-          <div className="ml-50 flex flex-col w-1/2">
+          <div className="flex flex-col w-1/2">
             <h2 className="text-md font-semibold mb-2">공시유형</h2>
             <div className="flex flex-wrap gap-2">
-              {[
-                "연간 보고서",
-                "분기 보고서",
-                "수시 보고서",
-                "증권 발행 등록 신고서",
-                "내부자 거래 보고서",
-                "주식 대량 보유 보고서",
-              ].map((type) => (
+              {fillingTypeData.map((type) => (
                 <button
-                  key={type}
+                  key={type.id}
                   className={`px-4 py-2 rounded-md ${
-                    selectedReportType === type
+                    fillingType === type.id
                       ? "bg-blue-md text-white"
                       : "bg-white"
                   }`}
-                  onClick={() => setSelectedReportType(type)}
+                  onClick={() => setFillingType(type.id)}
                 >
-                  {type}
+                  {type.name}
                 </button>
               ))}
             </div>
           </div>
         </div>
 
-        {/* 검색 버튼 (맨 아래 중앙 정렬) */}
-        <div className="flex justify-center mt-5">
-          <button className="w-100 bg-blue-md text-white px-6 py-2 rounded-md">
+        {/* 검색 버튼 */}
+        <div className="flex mt-8 w-100 gap-4">
+          <button className="button-style" onClick={handleSearch}>
             공시 검색하기
+          </button>
+          <button
+            className="white-button-style"
+            onClick={() => {
+              resetSearch();
+            }}
+          >
+            초기화
           </button>
         </div>
       </div>
@@ -134,11 +166,11 @@ export default function Index() {
                 <tr
                   key={index}
                   className="hover:bg-blue-100 cursor-pointer"
-                  onClick={() => handleClick(index)}
+                  onClick={() => handleClick(report.fillingId)}
                 >
-                  <td className="p-3">{report.type}</td>
-                  <td className="p-3">{report.title}</td>
-                  <td className="p-3">{report.date}</td>
+                  <td className="p-3">{report.fillingType}</td>
+                  <td className="p-3">{report.fillingTitle}</td>
+                  <td className="p-3">{report.submitTimestamp}</td>
                 </tr>
               ))}
             </tbody>
@@ -147,7 +179,10 @@ export default function Index() {
 
         {/* 페이지네이션 */}
         <div className="flex justify-center mt-4">
-          {[1, 2, 3].map((page) => (
+          {Array.from(
+            { length: Math.ceil(fillings.length / reportsPerPage) },
+            (_, i) => i + 1
+          ).map((page) => (
             <button
               key={page}
               className={`mx-1 px-3 py-1 rounded-md ${
