770df98692959c7ab8f2a69c210a6decb46f200d
ilkyo Yang <1004ilkyo@naver.com>
2025-03-03 20:57:25 +0900

    P3-87(#44) feature/공시 가이드 페이지   완료

--- src/pages/IntroduceDisclosurePage/index.jsx
+++ src/pages/IntroduceDisclosurePage/index.jsx
@@ -23,11 +23,21 @@ export default function Index() {
 
   return (
     <div className="flex flex-col items-center justify-center mx-24">
-      <div className="flex items-center mb-2 border-b-1 border-gray-md w-full justify-center pb-4">
-        <div className="text-3xl font-bold">공시 가이드 with</div>
-        <img src="../../../public/images/logo.png" className="h-16" />
+      <div className="w-full flex flex-col items-center pb-4">
+        <div className="flex items-center mb-2 w-full justify-center">
+          <div className="text-3xl font-bold">공시 가이드 with</div>
+          <img src="../../../public/images/logo.png" className="h-16" />
+        </div>
+  
+        <span className="text-[14px] text-blue-md text-center mt-1 mb-2">
+          <span>어떤 공시인지 모르겠다면? 핵심만 쏙쏙 알려드립니다. </span>
+          <br />
+          <span>투자에 필요한 중요한 공시 정보를 놓치지 마세요!</span>
+        </span>
+        <div className="w-full border-t-1 border-gray-md mt-4"></div>
       </div>
 
+      
       <div className=" grid grid-rows-2 p-4">
         {splitData.map((row, rowIndex) => (
           <div key={rowIndex} className="grid grid-cols-3 gap-4 p-2">
