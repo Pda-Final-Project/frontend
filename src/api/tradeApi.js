cc14707a5e45b059349076a2e035265b5b868190
kwak <sal091625@gmail.com>
2025-03-04 13:47:05 +0900

    P3-88(#45) feature: 메인창 실시간 주가 받기

--- src/api/tradeApi.js
+++ src/api/tradeApi.js
@@ -2,7 +2,7 @@
 import api from "./axiosInstance";
 
 const postOrder = (orderData) =>
-  api.post("http://127.0.0.1:19092/v1/api/order/create", orderData, {
+  api.post("http://127.0.0.1:19093/v1/api/order/create", orderData, {
     isAuthRequired: true,
   });
 
