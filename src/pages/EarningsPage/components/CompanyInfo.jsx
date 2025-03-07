e3ecca9e34457e45e1d3544546ebb3335ea176c3
kwak <sal091625@gmail.com>
2025-03-07 11:28:32 +0900

    임시

--- src/pages/EarningsPage/components/CompanyInfo.jsx
+++ src/pages/EarningsPage/components/CompanyInfo.jsx
@@ -11,8 +11,12 @@ const CompanyInfo = () => {
 
   // 현재 날짜 기준 가장 최근 실적 발표 데이터 찾기
   const today = new Date();
-  const pastReports = earningdata.filter((data) => new Date(data.date) <= today);
-  const latestReport = pastReports.sort((a, b) => new Date(b.date) - new Date(a.date))[0];
+  const pastReports = earningdata.filter(
+    (data) => new Date(data.date) <= today
+  );
+  const latestReport = pastReports.sort(
+    (a, b) => new Date(b.date) - new Date(a.date)
+  )[0];
 
   // 변동률(%) 계산 함수
   const calculatePercentageChange = (actual, estimated) => {
@@ -28,57 +32,64 @@ const CompanyInfo = () => {
   };
 
   const epsChange = latestReport
-    ? calculatePercentageChange(parseFloat(latestReport.eps), parseFloat(latestReport.eps_estimated))
+    ? calculatePercentageChange(
+        parseFloat(latestReport.eps),
+        parseFloat(latestReport.eps_estimated)
+      )
     : null;
 
   const revenueChange = latestReport
-    ? calculatePercentageChange(parseInt(latestReport.revenue), parseInt(latestReport.revenue_estimated))
+    ? calculatePercentageChange(
+        parseInt(latestReport.revenue),
+        parseInt(latestReport.revenue_estimated)
+      )
     : null;
 
   const formatToBillionWon = (amount) => {
-    return amount ? (parseInt(amount) / 1_0000_0000).toFixed(2) + "억원" : "N/A";
+    return amount
+      ? (parseInt(amount) / 1_0000_0000).toFixed(2) + "억원"
+      : "N/A";
   };
 
   return (
-    <div className="flex bg-gray-light p-2 rounded-lg items-center space-x-50 w-full h-full px-40">
-      
+    <div className="flex bg-gray-light p-2 px-8 justify-between rounded-lg items-center w-full h-full">
+      <div className="flex gap-4 items-center">
+        {/* 기업 로고 */}
+        <img
+          src={`${import.meta.env.VITE_STOCK_LOGO_URL}${ticker}.png`}
+          alt={`${ticker} 로고`}
+          className="w-8 h-8 object-contain rounded-full"
+        />
+        {/* 기업명 */}
+        <h2 className="text-lg font-semibold">{ticker}</h2>
+      </div>
+      <div className="flex gap-4 items-center text-sm font-semibold">
+        {/* EPS 정보 */}
+        {latestReport && (
+          <div className="flex items-center space-x-1">
+            <span>EPS |</span>
+            <span>{latestReport.eps ?? "N/A"}</span>
+            {epsChange !== null && (
+              <span className={`${getTextColor(epsChange)} text-sm`}>
+                ({epsChange}%)
+              </span>
+            )}
+          </div>
+        )}
 
-
-      {/* 기업 로고 */}
-      <img
-        src={`${import.meta.env.VITE_STOCK_LOGO_URL}${ticker}.png`}
-        alt={`${ticker} 로고`}
-        className="w-8 h-8 object-contain"
-      />
-
-      {/* 기업명 */}
-      <h2 className="text-lg font-semibold">{ticker}</h2>
-
-      {/* EPS 정보 */}
-      {latestReport && (
-        <div className="flex items-center space-x-1">
-          <span className="font-semibold">EPS:</span>
-          <span>{latestReport.eps ?? "N/A"}</span>
-          {epsChange !== null && (
-            <span className={`${getTextColor(epsChange)} text-sm`}>
-              ({epsChange}%)
-            </span>
-          )}
-        </div>
-      )}
-
-      {/* 매출 정보 */}
-      {latestReport && (
-        <div className="flex items-center space-x-1">
-          <span className="font-semibold">매출:</span>
-          <span>{formatToBillionWon(latestReport.revenue)}</span>
-          {revenueChange !== null && (
-            <span className={`${getTextColor(revenueChange)} text-sm`}>
-              ({revenueChange}%)
-            </span>
-          )}
-        </div>
-      )}
+        {/* 매출 정보 */}
+        {latestReport && (
+          <div className="flex items-center space-x-1">
+            <span>매출 |</span>
+            <span>{formatToBillionWon(latestReport.revenue)}</span>
+            {revenueChange !== null && (
+              <span className={`${getTextColor(revenueChange)} text-sm`}>
+                ({revenueChange}%)
+              </span>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/EarningsPage/index.jsx
+++ src/pages/EarningsPage/index.jsx
@@ -5,14 +5,17 @@ import EarningsChart from "./components/EarningsChart";
 
 const EarningsPage = () => {
   return (
-    
-    <div className="p-4">
-      <div className="flex justify-between text-lg font-bold">
-        <h1>실적 발표 및 어닝콜 정보</h1>
-        </div>
-      <h2 className="text-blue-md mt-2 mb-4 font-semibold">기업의 실적 발표 일정을 한눈에 확인하고, 예상 EPS 및 매출과 실제 실적을 비교해보세요. </h2>
+    <div className="px-32 py-20 flex flex-col w-full">
+      <div className="text-center">
+        <h1 className="text-lg font-bold">실적 발표 및 어닝콜 정보</h1>
+        <h2 className="text-blue-md mt-2 mb-4 font-semibold">
+          기업의 실적 발표 일정을 한눈에 확인하고, 예상 EPS 및 매출과 실제
+          실적을 비교해보세요.{" "}
+        </h2>
+      </div>
+
       {/* 기업 정보 */}
-      <div className="flex rounded-lg mb-5  mt-3">
+      <div className="flex rounded-lg mb-5 mt-3">
         <CompanyInfo />
       </div>
 
@@ -23,12 +26,15 @@ const EarningsPage = () => {
         </div>
 
         {/* 차트 */}
-        <h2 className="text-blue-md mt-2 mb-4 font-semibold">EPS 및 매출 변동 추이를 확인하고, 투자 결정을 위한 필수 정보를 쉽고 빠르게 확인하세요!</h2>
-        <div className=" bg-gray-light flex-col mb-10 rounded-lg justify-center">
-        <div className=" p-4 grid-cols-2 grid">
-        <h2 className="text-sm font-bold">분기별 EPS 변동 추이</h2>
-        <h2 className="text-sm font-bold">분기별 매출 변동 추이</h2>
-        </div>
+        <h2 className="text-blue-md mt-2 mb-4 font-semibold">
+          EPS 및 매출 변동 추이를 확인하고, 투자 결정을 위한 필수 정보를 쉽고
+          빠르게 확인하세요!
+        </h2>
+        <div className=" bg-gray-light flex-col p-8 rounded-lg justify-center">
+          <div className="grid-cols-2 grid gap-8 mb-4">
+            <h2 className="text-[16px] font-bold">분기별 EPS 변동 추이</h2>
+            <h2 className="text-[16px] font-bold">분기별 매출 변동 추이</h2>
+          </div>
           <EarningsChart />
         </div>
       </div>
