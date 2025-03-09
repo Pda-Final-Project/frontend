bc6c15df588c943760210289acb75fa474dafcd4
ilkyo Yang <1004ilkyo@naver.com>
2025-03-09 10:59:39 +0900

    P3-114(#59) feature/2025 상반기 실절발표 upcoming 일정 데이터 입력

--- src/pages/EarningsCalendarPage/EarningsCalendar.jsx
+++ src/pages/EarningsCalendarPage/EarningsCalendar.jsx
@@ -2,37 +2,40 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const earningsData = {
-  "3월": [
-    { ticker: "AAPL", date: "19일 17:00" },
-    { ticker: "TSLA", date: "22일 16:30" },
-    { ticker: "MSFT", date: "25일 18:00" },
-    { ticker: "GOOGL", date: "28일 17:45" },
-    { ticker: "AMZN", date: "30일 16:00" },
-    { ticker: "META", date: "31일 15:30" },
-    { ticker: "AMZN", date: "30일 16:00" },
-    { ticker: "META", date: "31일 15:30" },
-  ],
-  "4월": [
-    { ticker: "NFLX", date: "10일 17:00" },
-    { ticker: "NVDA", date: "12일 18:30" },
-  ],
-  "5월": [
-    { ticker: "NFLX", date: "10일 17:00" },
-    { ticker: "NVDA", date: "12일 18:30" },
-  ],
-  "6월": [
-    { ticker: "NFLX", date: "10일 17:00" },
-    { ticker: "NVDA", date: "12일 18:30" },
-  ],
-  "7월": [
-    { ticker: "NFLX", date: "10일 17:00" },
-    { ticker: "NVDA", date: "12일 18:30" },
-  ],
-  "8월": [
-    { ticker: "NFLX", date: "10일 17:00" },
-    { ticker: "NVDA", date: "12일 18:30" },
-  ],
-};
+    "3월": [
+      { ticker: "ORCL", date: "10일 17:00" },
+      { ticker: "ADBE", date: "12일 16:00" },
+    ],
+    "4월": [
+      { ticker: "META", date: "22일 06:00" },
+      { ticker: "NOW", date: "22일 06:00" },
+      { ticker: "TSLA", date: "23일 16:00" },
+      { ticker: "MSFT", date: "23일 16:00" },
+      { ticker: "GOOGL", date: "23일 16:00" },
+      { ticker: "IBM", date: "23일 16:00" },
+      { ticker: "INTC", date: "23일 16:00" },
+      { ticker: "V", date: "21일 06:00" },
+      { ticker: "NFLX", date: "16일 16:00" },
+      { ticker: "TSM", date: "16일 06:00" },
+      { ticker: "ASML", date: "16일 01:00" },
+      { ticker: "UNH", date: "14일 08:00" },
+      { ticker: "MS", date: "11일 08:00" },
+      { ticker: "JPM", date: "11일 08:00" },
+      { ticker: "XOM", date: "03일 17:00" },
+    ],
+    "5월": [
+      { ticker: "CRM", date: "27일 06:00" },
+      { ticker: "NVDA", date: "28일 16:00" },
+      { ticker: "COST", date: "28일 16:00" },
+      { ticker: "PLTR", date: "05일 06:00" },
+      { ticker: "WMT", date: "15일 09:00" },
+      { ticker: "LLY", date: "01일 08:00" },
+    ],
+    "6월": [
+      { ticker: "AVGO", date: "10일 16:00" },
+    ],
+  };
+  
 
 const EarningsCalendar = () => {
   const navigate = useNavigate();
@@ -43,15 +46,17 @@ const EarningsCalendar = () => {
 
   return (
     <div className="max-w-500 p-6 rounded-xl">
-      <h2 className="text-lg font-bold mb-1">실적발표 일정</h2>
-      <p className="text-blue-md font-semibold mb-4">
-        기업을 클릭하여 해당 기업의 분기 실적 보고 예상치와 실제 발표치를 확인해보세요!
+      
+      <h2 className="items-center text-lg text-center font-bold mb-1">2025년 상반기 실적발표 일정</h2>
+      
+      <p className="text-blue-md text-center font-semibold mb-4">
+        종목을 클릭하여 해당 기업의 분기 실적 보고 예상치와 실제 발표치를 확인해보세요!
       </p>
-      <div className="grid grid-cols-6 gap-3 bg-blue-md p-3 rounded-xl">
+      <div className="grid grid-cols-4 gap-3 bg-blue-md p-3 rounded-xl">
         {Object.entries(earningsData).map(([month, companies]) => (
           <div key={month} className="bg-white p-4 rounded-xl shadow-md">
             <h3 className="text-sm font-semibold bg-blue-light py-2 text-center rounded-md">{month}</h3>
-            <div className="grid grid-cols-2 gap-4 mt-4">
+            <div className="grid grid-cols-3 gap-4 mt-4">
               {companies.map((company, index) => (
                 <div 
                   key={index} 
