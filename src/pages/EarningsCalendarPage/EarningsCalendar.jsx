0b27c0135f1a36de931bdcb27ded8e834302dc40
ilkyo Yang <1004ilkyo@naver.com>
2025-03-10 09:44:35 +0900

    P3-114(#59)세로 스크롤 추가,  데이터 제공 출처 표시

--- src/pages/EarningsCalendarPage/EarningsCalendar.jsx
+++ src/pages/EarningsCalendarPage/EarningsCalendar.jsx
@@ -35,6 +35,14 @@ const EarningsCalendar = () => {
       { ticker: "ADBE", date: "12일 16:00" },
     ],
     "4월": [
+      { ticker: "XOM", date: "03일 17:00" },
+      { ticker: "MS", date: "11일 08:00" },
+      { ticker: "JPM", date: "11일 08:00" },
+      { ticker: "UNH", date: "14일 08:00" },
+      { ticker: "NFLX", date: "16일 16:00" },
+      { ticker: "TSM", date: "16일 06:00" },
+      { ticker: "ASML", date: "16일 01:00" },
+      { ticker: "V", date: "21일 06:00" },
       { ticker: "META", date: "22일 06:00" },
       { ticker: "NOW", date: "22일 06:00" },
       { ticker: "TSLA", date: "23일 16:00" },
@@ -42,52 +50,49 @@ const EarningsCalendar = () => {
       { ticker: "GOOGL", date: "23일 16:00" },
       { ticker: "IBM", date: "23일 16:00" },
       { ticker: "INTC", date: "23일 16:00" },
-      { ticker: "V", date: "21일 06:00" },
-      { ticker: "NFLX", date: "16일 16:00" },
-      { ticker: "TSM", date: "16일 06:00" },
-      { ticker: "ASML", date: "16일 01:00" },
-      { ticker: "UNH", date: "14일 08:00" },
-      { ticker: "MS", date: "11일 08:00" },
-      { ticker: "JPM", date: "11일 08:00" },
-      { ticker: "XOM", date: "03일 17:00" },
+      { ticker: "KO", date: "28일 08:00" },
+      { ticker: "AMZN", date: "28일 06:00" },
+      { ticker: "AMD", date: "28일 06:00" },
+      { ticker: "MA", date: "29일 08:00" },
       { ticker: "AAPL", date: "30일 06:00" },
       { ticker: "LIN", date: "30일 06:00" },
-      { ticker: "KO", date: "28일 08:00" },
-      { ticker: "AMD", date: "28일 06:00" },
     ],
     "5월": [
+      { ticker: "LLY", date: "01일 08:00" },
+      { ticker: "PLTR", date: "05일 06:00" },
+      { ticker: "WMT", date: "15일 09:00" },
       { ticker: "CRM", date: "27일 06:00" },
       { ticker: "NVDA", date: "28일 16:00" },
       { ticker: "COST", date: "28일 16:00" },
-      { ticker: "PLTR", date: "05일 06:00" },
-      { ticker: "WMT", date: "15일 09:00" },
-      { ticker: "LLY", date: "01일 08:00" },
     ],
     "6월": [
       { ticker: "AVGO", date: "10일 16:00" },
     ],
-
   };
   
   return (
+    
     <div className="max-w-500 p-6 rounded-xl">
-      
       <h2 className="items-center text-lg text-center font-bold mb-1">2025년 상반기 실적발표 일정</h2>
       
       <p className="text-blue-md text-center text-sm font-semibold mb-4">
         종목을 클릭하여 해당 기업의 분기 실적 보고 예상치와 실제 발표치를 확인해보세요!
       </p>
-      <div className="text-center text-sm font-bold p-2rounded-md mb-2">
+      <p className="bg-blue-light mr-100 ml-100 text-center text-sm font-semibold p-2 rounded-md mb-2">
         뉴욕 현재 시간 {nyTime} (EDT/EST)
-      </div>
+      </p>
       <p className="text-blue-md text-right font-semibold mb-2">
       아래 일정은 뉴욕시간 기준이며, 매주 업데이트 됩니다.
       </p>
       <div className="grid grid-cols-4 gap-3 bg-blue-md p-3 rounded-xl">
         {Object.entries(earningsData).map(([month, companies]) => (
-          <div key={month} className="bg-white p-4 rounded-xl shadow-md">
+            
+          <div >
             <h3 className="text-sm font-semibold bg-blue-light py-2 text-center rounded-md">{month}</h3>
-            <div className="grid grid-cols-4 gap-4 mt-4">
+            <div className="bg-white p-4 rounded-xl shadow-md mt-3 h-100 overflow-y-auto">
+                
+            
+            <div className="grid grid-cols-3 gap-4 mt-4">
               {companies.map((company, index) => (
                 <div 
                   key={index} 
@@ -97,15 +102,26 @@ const EarningsCalendar = () => {
                   <img
                     src={`${import.meta.env.VITE_STOCK_LOGO_URL}${company.ticker}.png`}
                     alt={company.ticker}
-                    className="w-12 h-12 rounded-full border border-gray-300"
+                    className="w-12 h-12 rounded-full border border-gray-300 transition-transform duration-300 ease-in-out hover:scale-130"
                   />
                   <p className="text-sm mt-2">{company.date}</p>
+                  
                 </div>
+                
               ))}
             </div>
+            
+            </div>
+            
+        
           </div>
+          
+         
         ))}
       </div>
+      <p className="text-center text-xs text-blue-md font-semibold bg-blue-light p-2 rounded-xl mt-2 mr-100 ml-100">
+  데이터 제공: <a href="https://finance.yahoo.com/calendar" target="_blank" className="underline">Yahoo! Finance Earnings Calendar</a>
+</p>
     </div>
   );
 };
