444de3f7d5407fc2a00dd84814ea8abad62aa35b
kwak <sal091625@gmail.com>
2025-03-10 10:03:49 +0900

    P3-116(#63) [FEAT] 알람 api 함수 작성

--- src/api/alarmApi.js
+++ src/api/alarmApi.js
@@ -1,1 +1,16 @@
 //알림 조회, 알림 on/off
+import api from "./axiosInstance";
+
+//알림 조회
+const fetchAlarm = () =>
+  api.get("http://127.0.0.1:19092/v1/api/notification", {
+    isAuthRequired: true,
+  });
+
+//알림 켜고 끄기
+const changeAlarmStatus = (enabled) =>
+  api.patch(`http://127.0.0.1:19092/v1/api/notification?enabled=${enabled}`, {
+    isAuthRequired: true,
+  });
+
+export { fetchAlarm, changeAlarmStatus };
