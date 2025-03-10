2b0a9691276b3f2985872036945bf49b9d1a7253
kwak <sal091625@gmail.com>
2025-03-10 12:56:52 +0900

    P3-116(#63) [FEAT] 임시

--- src/pages/LandingPage/components/Disclosures.jsx
+++ src/pages/LandingPage/components/Disclosures.jsx
@@ -53,56 +53,58 @@ export default function Disclosures() {
           더 많은 공시 보러가기
         </span>
       </div>
+      <div className="w-full relative">
+        {/* 오른쪽 끝에 고정된 그림자 추가 */}
+        <div className="absolute top-0 right-0 bottom-0 w-24 bg-gradient-to-l from-[rgba(255,255,255,0.8)] to-transparent pointer-events-none z-10"></div>
+        <div className="w-full overflow-x-scroll relative no-scrollbar">
+          <div className="flex space-x-4 rounded-lg max-w-screen-xl whitespace-nowrap shadow-lg">
+            {/* fillings 배열이 제대로 있는지 확인하고, 없다면 로딩 중 표시 */}
+            {Array.isArray(fillings) && fillings.length > 0 ? (
+              fillings.map((item, index) => (
+                <div
+                  key={index}
+                  className="bg-gray-50 p-4 rounded-lg shadow-light transition-all flex flex-col cursor-pointer hover:bg-gray-200 duration-300 min-w-60"
+                  onClick={() =>
+                    navigate(`/main/${item.fillingTicker}/${item.fillingId}`)
+                  } // 종목코드(티커) 클릭 시 이동
+                >
+                  {/* 제목 */}
+                  <p className="font-bold text-lg sm:text-[16px]">
+                    {item.fillingTitle}
+                  </p>
 
-      {/* 가로 스크롤 컨테이너 */}
-      <div className="w-full overflow-auto no-scrollbar">
-        <div className="flex space-x-4 rounded-lg max-w-screen-xl whitespace-nowrap">
-          {/* fillings 배열이 제대로 있는지 확인하고, 없다면 로딩 중 표시 */}
-          {Array.isArray(fillings) && fillings.length > 0 ? (
-            fillings.map((item, index) => (
-              <div
-                key={index}
-                className="bg-gray-50 p-4 rounded-lg shadow-light transition-all flex flex-col cursor-pointer hover:bg-gray-200 duration-300 min-w-60"
-                onClick={() =>
-                  navigate(`/main/${item.fillingTicker}/${item.fillingId}`)
-                } // 종목코드(티커) 클릭 시 이동
-              >
-                {/* 제목 */}
-                <p className="font-bold text-lg sm:text-[16px]">
-                  {item.fillingTitle}
-                </p>
+                  {/* 공시 ID */}
+                  <p className="text-[12px] sm:text-base text-gray-600 mt-1">
+                    {item.fillingId}
+                  </p>
 
-                {/* 공시 ID */}
-                <p className="text-[12px] sm:text-base text-gray-600 mt-1">
-                  {item.fillingId}
-                </p>
+                  {/* 공시 타입 & 종목 코드 (클릭 가능하게) */}
+                  <p className="text-[10px] sm:text-base mt-1 font-medium">
+                    {item.fillingType} •{" "}
+                    <span className="text-blue-md font-bold cursor-pointer hover:underline">
+                      {item.fillingTicker}
+                    </span>
+                  </p>
 
-                {/* 공시 타입 & 종목 코드 (클릭 가능하게) */}
-                <p className="text-[10px] sm:text-base mt-1 font-medium">
-                  {item.fillingType} •{" "}
-                  <span className="text-blue-md font-bold cursor-pointer hover:underline">
-                    {item.fillingTicker}
-                  </span>
-                </p>
+                  {/* 생성 일시 */}
+                  <p className="text-sm text-gray-500 mt-auto">
+                    {new Date(item.submitTimestamp).toLocaleString()}
+                  </p>
+                </div>
+              ))
+            ) : (
+              <p>로딩 중...</p> // fillings 데이터가 없을 때 로딩 표시
+            )}
 
-                {/* 생성 일시 */}
-                <p className="text-sm text-gray-500 mt-auto">
-                  {new Date(item.submitTimestamp).toLocaleString()}
-                </p>
-              </div>
-            ))
-          ) : (
-            <p>로딩 중...</p> // fillings 데이터가 없을 때 로딩 표시
-          )}
-
-          {/* 더보기 버튼 */}
-          <button
-            onClick={() => navigate("/disclosures")}
-            className="flex items-center justify-center min-w-60 bg-gray-50 text-blue-md font-bold text-lg rounded-lg shadow-md transition-all hover:bg-gray-200 cursor-pointer duration-300"
-          >
-            <HiPlusCircle />
-            <span className="ml-1">more</span>
-          </button>
+            {/* 더보기 버튼 */}
+            <button
+              onClick={() => navigate("/disclosures")}
+              className="flex items-center justify-center min-w-60 bg-gray-50 text-blue-md font-bold text-lg rounded-lg shadow-md transition-all hover:bg-gray-200 cursor-pointer duration-300"
+            >
+              <HiPlusCircle />
+              <span className="ml-1">more</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/LandingPage/components/News.jsx
+++ src/pages/LandingPage/components/News.jsx
@@ -48,49 +48,52 @@ export default function News() {
           뉴스 데이터를 불러오는 중...
         </p>
       ) : (
-        <div className="overflow-auto no-scrollbar">
-          {" "}
-          {/* 가로 스크롤 적용 */}
-          <div className="flex space-x-4 p-4 rounded-lg max-w-screen-xl">
-            {newsData.slice(0, 10).map((article, index) => (
+        <div className="w-full relative">
+          <div className="absolute top-0 right-0 bottom-0 w-24 bg-gradient-to-l from-[rgba(255,255,255,0.8)] to-transparent pointer-events-none z-10"></div>
+
+          <div className="overflow-auto no-scrollbar">
+            {/* 가로 스크롤 적용 */}
+            <div className="flex space-x-4 p-4 rounded-lg max-w-screen-xl">
+              {newsData.slice(0, 10).map((article, index) => (
+                <a
+                  key={index}
+                  href={article.news_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="overflow-hidden rounded-lg hover:shadow-md transition-shadow w-72 flex flex-col duration-300 h-60">
+                    {/* 뉴스 이미지 */}
+                    <img
+                      src={article.news_img.replace(/S\.jpg$/, "L.jpg")}
+                      alt={article.news_title}
+                      className="w-72 h-40 object-cover rounded-md transition-transform hover:scale-110"
+                    />
+
+                    {/* 뉴스 제목 */}
+                    <h2 className="mt-2 font-semibold text-sm px-2">
+                      {article.news_title && article.news_title.length > 53
+                        ? article.news_title.slice(0, 53) + "..."
+                        : article.news_title}
+                    </h2>
+                    {/* 출처 및 시간 ("00분 전" 표시) */}
+                    <p className="text-md text-gray-600 px-2 py-1">
+                      {article.news_company} • {timeAgo(article.news_date)}
+                    </p>
+                  </div>
+                </a>
+              ))}
+
+              {/* 더보기 버튼 */}
               <a
-                key={index}
-                href={article.news_url}
+                href={NEWS_SITE_URL}
                 target="_blank"
                 rel="noopener noreferrer"
+                className="flex items-center justify-center min-w-72 bg-gray-50 text-blue-md font-bold text-lg rounded-lg transition-all hover:bg-gray-200 duration-300"
               >
-                <div className="overflow-hidden rounded-lg hover:shadow-md transition-shadow w-72 flex flex-col duration-300 h-60">
-                  {/* 뉴스 이미지 */}
-                  <img
-                    src={article.news_img.replace(/S\.jpg$/, "L.jpg")}
-                    alt={article.news_title}
-                    className="w-72 h-40 object-cover rounded-md transition-transform hover:scale-110"
-                  />
-
-                  {/* 뉴스 제목 */}
-                  <h2 className="mt-2 font-semibold text-sm px-2">
-                    {article.news_title && article.news_title.length > 53
-                      ? article.news_title.slice(0, 53) + "..."
-                      : article.news_title}
-                  </h2>
-                  {/* 출처 및 시간 ("00분 전" 표시) */}
-                  <p className="text-md text-gray-600 px-2 py-1">
-                    {article.news_company} • {timeAgo(article.news_date)}
-                  </p>
-                </div>
+                <HiPlusCircle />
+                <span className="ml-1">more</span>
               </a>
-            ))}
-
-            {/* 더보기 버튼 */}
-            <a
-              href={NEWS_SITE_URL}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center justify-center min-w-72 bg-gray-50 text-blue-md font-bold text-lg rounded-lg transition-all hover:bg-gray-200 duration-300"
-            >
-              <HiPlusCircle />
-              <span className="ml-1">more</span>
-            </a>
+            </div>
           </div>
         </div>
       )}
