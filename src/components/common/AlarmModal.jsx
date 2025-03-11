06bd2411fec6b37e2634ee0bdee23eb485348b2c
kwak <sal091625@gmail.com>
2025-03-11 09:19:02 +0900

    P3-116(#63) [FEAT] 알림 내역 조회 UI 구현 및 api 연동

--- src/components/common/AlarmModal.jsx
+++ src/components/common/AlarmModal.jsx
@@ -8,7 +8,7 @@ const AlarmModal = ({ onClose }) => {
 
   const tryChangeAlarmStatus = async () => {
     try {
-      const response = await changeAlarmStatus(!isAlarmOn); // 상태를 반전시켜 보내기
+      const response = await changeAlarmStatus(!isAlarmOn);
       if (response.data.status === "OK") {
         setIsAlarmOn(!isAlarmOn);
       }
@@ -20,9 +20,10 @@ const AlarmModal = ({ onClose }) => {
   const tryFetchAlarm = async () => {
     try {
       const response = await fetchAlarm();
-      if (response.data.status == "OK") {
-        setAlarms(response.data.data);
-      }
+      const parsedAlarms = response.data.map((alarm) => ({
+        ...JSON.parse(alarm.data),
+      }));
+      setAlarms(parsedAlarms);
     } catch (error) {
       console.error(error.message);
     }
@@ -33,17 +34,16 @@ const AlarmModal = ({ onClose }) => {
   }, []);
 
   return (
-    <div className="fixed top-12 right-0 m-4 w-92 bg-white shadow-lg rounded-lg p-3">
-      {/** 알람 모달 헤더 */}
+    <div className="fixed top-12 right-0 m-4 w-96 bg-white shadow-lg rounded-lg p-3">
+      {/* 알람 모달 헤더 */}
       <div className="flex justify-between items-center pb-2 mb-2">
         <div className="flex items-center">
           <h2 className="text-lg font-semibold p-3">알림</h2>
-          {/* 알림 상태 toggle 버튼 추가 */}
-
+          {/* 알림 상태 toggle 버튼 */}
           <div
             onClick={tryChangeAlarmStatus}
             className={`relative w-12 h-6 flex items-center px-1 rounded-full cursor-pointer transition-all duration-300 ${
-              isAlarmOn ? "bg-blue-md" : "bg-gray-400"
+              isAlarmOn ? "bg-blue-md" : "bg-gray-md"
             }`}
           >
             <div
@@ -53,56 +53,46 @@ const AlarmModal = ({ onClose }) => {
             ></div>
           </div>
         </div>
-
         <button
           onClick={onClose}
-          className="text-sm font-bold text-blue-md hover:text-gray-500 duration-300 p-3"
+          className="text-sm font-bold text-blue-600 hover:text-gray-500 duration-300 p-3"
         >
           ✕
         </button>
       </div>
-      {/** 알람 모달 바디 */}
-      <div className="space-y-4">
+
+      {/* 알람 모달 바디 */}
+      <div className="space-y-4 h-full max-h-[300px] overflow-auto no-scrollbar text-[14px]">
         {alarms.length > 0 ? (
           alarms.map((alarm, index) => (
-            <div key={index} className="bg-blue-50 p-4 rounded-lg">
-              {alarm.type === "trade" ? (
-                <div>
-                  <p className="font-bold">[해외주식 매수 주문 체결]</p>
-                  <p className="text-gray-700 text-sm">
-                    {timeAgo(alarm.time)} 전
-                  </p>
-                  <p>
-                    종목명{" "}
-                    <span className="font-semibold">{alarm.stockName}</span>
-                  </p>
-                  <p>
-                    주문 수량{" "}
-                    <span className="font-semibold">{alarm.orderQty}주</span>
-                  </p>
-                  <p>
-                    체결 수량{" "}
-                    <span className="font-semibold">{alarm.filledQty}주</span>
-                  </p>
-                  <p>
-                    체결 가격{" "}
-                    <span className="font-semibold">
-                      {alarm.price.toFixed(3)} USD
-                    </span>
-                  </p>
-                </div>
-              ) : (
-                <div>
-                  <p className="font-bold">New 공시 알림</p>
-                  <p className="text-gray-700 text-sm">
-                    {timeAgo(alarm.time)} 전
-                  </p>
-                  <p>{alarm.message}</p>
-                  <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg">
-                    공시 보러가기
-                  </button>
-                </div>
-              )}
+            <div
+              key={index}
+              className="bg-blue-50 p-4 rounded-lg flex flex-col"
+            >
+              <p className="font-bold mb-2">{alarm.title}</p>
+
+              <div className="grid grid-cols-2">
+                <p>
+                  종목명:{" "}
+                  <span className="font-semibold">{alarm.stockTicker}</span>
+                </p>
+                <p>
+                  주문 수량:{" "}
+                  <span className="font-semibold">{alarm.orderQuantity}주</span>
+                </p>
+              </div>
+              <div className="grid grid-cols-2">
+                <p>
+                  체결 수량:{" "}
+                  <span className="font-semibold">{alarm.tradeQuantity}주</span>
+                </p>
+                <p>
+                  체결 가격:{" "}
+                  <span className="font-semibold">
+                    {alarm.tradePrice.toFixed()}원
+                  </span>
+                </p>
+              </div>
             </div>
           ))
         ) : (
