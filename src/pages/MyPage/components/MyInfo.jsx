e8e764e024a43020634c96840bba0dd84ade21f1
ilkyo Yang <1004ilkyo@naver.com>
2025-03-11 16:11:14 +0900

    P3-117(#66) feature/예수금 조회 모달창 ui 구현

--- src/pages/MyPage/components/MyInfo.jsx
+++ src/pages/MyPage/components/MyInfo.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { fetchAccountInfo } from "../../../api/accountApi";
+import Deposit from "./Deposit";
 
 export default function MyInfo() {
   const [account, setAccount] = useState({
@@ -8,10 +9,12 @@ export default function MyInfo() {
     userName: "홍길동",
   });
 
+  const [isDepositOpen, setIsDepositOpen] = useState(false);
+
   const tryFetchAccount = async () => {
     try {
-      const response = await fetchAccountInfo(); // 실제 API URL로 변경
-      if (response.data.status == "OK") {
+      const response = await fetchAccountInfo();
+      if (response.data.status === "OK") {
         setAccount(response.data.data);
       }
     } catch (error) {
@@ -20,23 +23,30 @@ export default function MyInfo() {
   };
 
   useEffect(() => {
-    tryFetchAccount(); // 컴포넌트 마운트 시 API 호출 (현재 주석 처리)
+    tryFetchAccount();
   }, []);
 
   return (
     <div className="w-full flex justify-between items-center bg-gray-light p-3 rounded-lg font-semibold">
       <div>
-              {/* 계좌번호 */}
-      <span className="text-[16px]">{account.account_number}</span>
-      <span className="bg-blue-100 p-2 rounded-lg ml-3 hover:cursor-pointer hover:bg-blue-md hover:text-white">
-        예수금 조회
-      </span>
+        {/* 계좌번호 */}
+        <span className="text-[16px]">{account.accountNumber}</span>
+        <button 
+          className="bg-blue-100 p-2 rounded-lg ml-3 hover:cursor-pointer hover:bg-blue-md hover:text-white"
+          onClick={() => setIsDepositOpen(true)}
+        >
+          예수금 조회
+        </button>
       </div>
+
       {/* 계좌 타입 & 사용자 이름 */}
       <div className="text-[16px] flex space-x-2">
         <span>{account.accountName}</span>
         <span>{account.userName}</span>
       </div>
+
+      {/* 예수금 조회 모달 */}
+      <Deposit isOpen={isDepositOpen} onClose={() => setIsDepositOpen(false)} />
     </div>
   );
 }
