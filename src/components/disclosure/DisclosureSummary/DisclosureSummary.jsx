c7276ffe06998d7cc2c5b1957b240a290cc4aee9
ilkyo Yang <1004ilkyo@naver.com>
2025-03-12 09:34:05 +0900

    P3-82(#37) feature/summary 컴포넌트 ui 호버시 전체 보기 기능  추가, summary 컴포넌트 없을 시  예외처리

--- src/components/disclosure/DisclosureSummary/DisclosureSummary.jsx
+++ src/components/disclosure/DisclosureSummary/DisclosureSummary.jsx
@@ -9,6 +9,14 @@ export default function Summary({ summaryData }) {
       ? JSON.parse(summaryData.trim())
       : summaryData;
 
+  // 추가 정보 추출
+  const marketImpact = data.summary["시장 영향"];
+  const riskFactor = data.summary["주요 리스크 요인"];
+  const outlook = data.summary["장단기 전망"];
+
+  // 추가 요약 항목들이 하나라도 존재하는지 확인
+  const hasExtraSummary = marketImpact || riskFactor || outlook;
+
   return (
     <div className="p-8">
       <div className="summary-container text-[14px] w-full flex-col">
@@ -45,51 +53,53 @@ export default function Summary({ summaryData }) {
         <h2 className="text-[20px] font-semibold mb-4">총평</h2>
         <p
           className="p-6 border border-gray-md rounded-lg text-[14px] bg-gray-light"
-          dangerouslySetInnerHTML={{ __html: data.summary["전체 총평"] }}
+          dangerouslySetInnerHTML={{ __html: data.summary["총평"] }}
         ></p>
 
-        <div className="space-y-4 flex-col mt-4">
-          <div className="flex gap-4">
-            {/* 성장 요인 */}
-            <div className="w-full flex-col items-start space-y-2 p-4 border border-blue-md rounded-lg text-blue-600 transition-transform hover:scale-105">
-              <div className="flex gap-2 items-center">
-                <FiTrendingUp className="h-[16px] w-[16px] " />
-                <h4 className="font-semibold">주요 성장 요인</h4>
-              </div>
-              <p
-                className="text-sm"
-                dangerouslySetInnerHTML={{
-                  __html: data.summary["주요 성장 요인"],
-                }}
-              ></p>
-            </div>
-            {/* 리스크 요인 */}
-            <div className="w-full flex-col items-start p-4 space-y-2 border border-red-500 rounded-lg transition-transform hover:scale-105">
-              <div className="flex items-center gap-2">
-                <FiAlertCircle className="h-[16px] w-[16px] text-red-700" />
-                <h4 className="font-semibold text-red-700">주요 리스크 요인</h4>
-              </div>
+        {hasExtraSummary && (
+          <div className="space-y-4 flex-col mt-4">
+            <div className="flex gap-4">
+              {marketImpact && (
+                <div className="w-full flex-col items-start space-y-2 p-4 border border-blue-400 rounded-lg transition-transform hover-container hover: duration-300 hover:scale-105">
+                  <div className="flex gap-2 items-center">
+                    <FiTrendingUp className="h-[16px] w-[16px] text-blue-700" />
+                    <h4 className="font-semibold text-blue-700">시장 영향</h4>
+                  </div>
+                  <p
+                    className="text-sm text-blue-700 hover-expand"
+                    dangerouslySetInnerHTML={{ __html: marketImpact }}
+                  ></p>
+                </div>
+              )}
 
-              <p
-                className="text-sm text-red-700"
-                dangerouslySetInnerHTML={{
-                  __html: data.summary["주요 리스크 요인"],
-                }}
-              ></p>
-            </div>
-            {/* 전망 */}
-            <div className="w-full flex-col items-start space-y-2 p-4 border border-green-600 rounded-lg transition-transform hover:scale-105">
-              <div className="flex items-center gap-2">
-                <FiCheckCircle className="h-[16px] w-[16px] text-green-600" />
-                <h4 className="font-semibold text-green-600">전망</h4>{" "}
-              </div>
-              <p
-                className="text-sm text-green-600"
-                dangerouslySetInnerHTML={{ __html: data.summary["전망"] }}
-              ></p>
+              {riskFactor && (
+                <div className="w-full flex-col items-start p-4 space-y-2 border border-red-400 rounded-lg transition-transform hover-container hover: duration-300 hover:scale-105">
+                  <div className="flex items-center gap-2">
+                    <FiAlertCircle className="h-[16px] w-[16px] text-red-700" />
+                    <h4 className="font-semibold text-red-700">주요 리스크 요인</h4>
+                  </div>
+                  <p
+                    className="text-sm text-red-700 hover-expand"
+                    dangerouslySetInnerHTML={{ __html: riskFactor }}
+                  ></p>
+                </div>
+              )}
+
+              {outlook && (
+                <div className="w-full flex-col items-start space-y-2 p-4 border border-green-400 rounded-lg transition-transform hover-container hover: duration-300 hover:scale-105">
+                  <div className="flex items-center gap-2">
+                    <FiCheckCircle className="h-[16px] w-[16px] text-green-600" />
+                    <h4 className="font-semibold text-green-600">장단기 전망</h4>
+                  </div>
+                  <p
+                    className="text-sm text-green-600 hover-expand"
+                    dangerouslySetInnerHTML={{ __html: outlook }}
+                  ></p>
+                </div>
+              )}
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
