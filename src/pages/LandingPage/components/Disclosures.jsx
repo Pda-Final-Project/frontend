168678cf96705cfbe76c1de400d231bf04f5794f
kwak <sal091625@gmail.com>
2025-03-16 17:31:38 +0900

    P3-122(#74) [FIX] 랜딩 length 에러 핸들링

--- src/pages/LandingPage/components/Disclosures.jsx
+++ src/pages/LandingPage/components/Disclosures.jsx
@@ -5,7 +5,7 @@ import { fetchFillings } from "../../../api/disclosureApi";
 
 export default function Disclosures() {
   const navigate = useNavigate();
-  const [fillings, setFillings] = useState(); // 초기값을 더미 데이터로 설정
+  const [fillings, setFillings] = useState([]); // 초기값을 더미 데이터로 설정
 
   useEffect(() => {
     tryFetchFillings();
@@ -45,7 +45,7 @@ export default function Disclosures() {
         <div className="w-full overflow-x-scroll relative no-scrollbar">
           <div className="flex space-x-4 rounded-lg max-w-xl whitespace-nowrap shadow-lg">
             {/* fillings 배열이 제대로 있는지 확인하고, 없다면 로딩 중 표시 */}
-            {Array.isArray(fillings) && fillings?.length > 0 ? (
+            {Array.isArray(fillings) && fillings.length > 0 ? (
               fillings?.map((item, index) => (
                 <div
                   key={index}
